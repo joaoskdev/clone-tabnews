1084a5b0a09d84ea853c9df8bc079d3283c78fdc
Joao Sabakeviski <jh.sabakeviski@gmail.com>
2026-01-08 10:29:22 +0000

    adiciona o arquivo .gitignore e ajusta o index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Teste</h1>;
+  return (
+    <h1>
+      "A existência é uma imperfeição que o nada acaba por corrigir." — Emil
+      Cioran
+    </h1>
+  );
 }
 
 export default Home;
