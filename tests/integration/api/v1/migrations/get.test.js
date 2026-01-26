725cd6ea21bed4af339c41a63bf75eace21c5e53
Joao Sabakeviski <jh.sabakeviski@gmail.com>
2026-01-26 00:13:09 +0000

    teste ambiente de homologacao

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -13,4 +13,5 @@ test("GET to /api/v1/migrations should return 200", async () => {
 
   expect(Array.isArray(responseBody)).toBe(true);
   expect(responseBody.length).toBeGreaterThan(0);
+  console.log("teste");
 });
