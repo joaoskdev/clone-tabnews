eb858ac15278807fc860e9770dc750f42ffe6c20
Joao Sabakeviski <jh.sabakeviski@gmail.com>
2026-01-27 19:17:13 +0000

    makes `npm test` more robust with async-retry and `orchestrator.js`

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -10,7 +10,7 @@ function checkPostgres() {
       return;
     }
 
-    console.log("\n\n🟢 Postgres está pronto e aceitando conexões");
+    console.log("\n🟢 Postgres está pronto e aceitando conexões");
   }
 }
 
