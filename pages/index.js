611bd357b85739ff1f65f501bb8c8d9b2a44410d
Joao Sabakeviski <jh.sabakeviski@gmail.com>
2026-01-29 14:42:07 +0000

    adds `eslint` job in `Linting` GitHub Action

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 function Home() {
   return (
     <h1>
-      "A Existência é uma imperfeição que o nada acaba por corrigir." — Emil
-      Cioran
+      {
+        "A Existência é uma imperfeição que o nada acaba por corrigir. — Emil Cioran"
+      }
     </h1>
   );
 }
